4921b670cfb20d0d2ed9410241bad7a94851c438
Leannon <118322766+LEANN0N@users.noreply.github.com>
2023-03-20 20:49:47 -0300

    Add files via upload

---
+++ src/Matriz.hpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#ifndef MATRIZ_HPP_INCLUDED
+#define MATRIZ_HPP_INCLUDED
+
+using namespace std;
+
+class Matriz
+{
+  private:
+    int **matriz;
+
+  public:
+    
+	  Matriz(int **matriz);
+    Matriz();
+
+    void setMatriz(int **matriz);
+    int **getMatriz();
+
+    int **criarMatriz(int **matriz, int tam);
+    int **limparMatriz(int **matriz, int tam);
+    int percorrerMatriz(int **matriz, int tam);
+};
+#endif
